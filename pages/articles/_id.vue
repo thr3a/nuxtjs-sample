303114260748ead4aa0147c8c6683ee150c6e128
thr3a <12jk172@ms.dendai.ac.jp>
2018-05-05 15:41:45 +0900

    update: ID動的表示

---
+++ pages/articles/_id.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>{{ id }}</h1>
+</template>
+
+
+<script>
+// こういう書き方
+export default {
+  asyncData({params}) {
+    const {id} = params
+    return {
+      id
+    }
+  }
+}
+</script>
---
+++ pages/articles/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>articles</h1>
+</template>
